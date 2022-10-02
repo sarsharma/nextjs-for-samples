c5681dfb88eb79dd0e6549ea641cfdc110120257
Vivek Jilla <vivekjilla7@gmail.com>
2022-10-02 14:54:31 +0530

    Update index.js

--- examples/hello-world/pages/index.js
+++ examples/hello-world/pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function IndexPage() {
   return (
     <div>
-      Hello World.{' '}
+      Hello Worlds.{' '}
       <Link href="/about">
         <a>About</a>
       </Link>
